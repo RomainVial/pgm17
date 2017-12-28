7871bfc028d1118d1a9d9f8195570477d17351f2
Romain Vial <romain.vial@mines-paristech.fr>
2017-12-28 19:21:32 +0100

    first commit hw3

---
+++ assignment3/clustering.py
@@ -0,0 +1,260 @@
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+import copy
+
+mpl.rcParams.update({'font.size': 30})
+
+def plot_ellipse(splot, mean, cov, color):
+    v, w = np.linalg.eigh(cov)
+    u = w[0] / np.linalg.norm(w[0])
+    angle = np.arctan(u[1] / u[0])
+    angle = 180 * angle / np.pi  # convert to degrees
+    # filled Gaussian at 2 standard deviation
+    ell = mpl.patches.Ellipse(mean, 2 * v[0] ** 0.5, 2 * v[1] ** 0.5,
+                              180 + angle, facecolor=color,
+                              edgecolor='yellow',
+                              linewidth=2, zorder=2)
+    ell.set_clip_box(splot.bbox)
+    ell.set_alpha(0.5)
+    splot.add_artist(ell)
+
+
+class KMeans:
+    def __init__(self, K, initializer='zero'):
+        self.K = K
+        self.mu = None
+        self.maxstep = 100
+        self.nb_restart = 10
+        if initializer == 'zero':
+            self.initialize = self.zero_initializer
+        elif initializer == 'random':
+            self.initialize = self.random_initializer
+        elif initializer == 'random_from_data':
+            self.initialize = self.random_from_data_initializer
+
+    def zero_initializer(self, X):
+        return np.zeros((self.K, X.shape[1]))
+
+    def random_initializer(self, X):
+        x_min, x_max = np.min(X[:,0]), np.max(X[:,0])
+        y_min, y_max = np.min(X[:, 1]), np.max(X[:, 1])
+        return [(x_max - x_min), (y_max - y_min)] * np.random.rand(self.K, X.shape[1]) + [x_min, y_min]
+
+    def random_from_data_initializer(self, X):
+        indexes = np.random.choice(X.shape[0], size=self.K, replace=False)
+        return X[indexes,:]
+
+    def distortion(self, X, z, mu):
+        J = 0
+        for k in range(self.K):
+            J += np.sum(np.square(X[z == k,:] - mu[k,:]))
+        return J
+
+    def train(self, X, render=True):
+        J = [ [] for _ in range(self.nb_restart) ]
+        mu = np.zeros((self.nb_restart, self.K, X.shape[1]))
+        for restart in range(self.nb_restart):
+            mu[restart,:] = self.initialize(X)
+            mu_old = copy.copy(mu[restart,:])
+            term = False
+            step = 0
+            while step < self.maxstep and not term:
+                # Compute distances then assign to corresponding centroids
+                distances = np.sum(np.square(np.expand_dims(X, axis=1) - mu[restart]), axis=-1)
+                z = np.argmin(distances, axis=-1)
+
+                # Compute distortion
+                J[restart].append(self.distortion(X, z, mu[restart]))
+
+                # Update the centroids based on the assignment
+                for k in range(self.K):
+                    mu[restart,k] = np.sum(X[z == k,:], axis=0) / (np.sum(z == k) + 1e-12)
+
+                # Terminate the computation if we reach a stationary point
+                term = np.all(np.equal(mu[restart], mu_old))
+                mu_old = copy.copy(mu[restart])
+                step += 1
+        best_restart = np.argmin(J[restart][-1] for restart in range(self.nb_restart))
+        self.mu = mu[best_restart]
+
+        if render:
+            print 'Fitted after {} iterations'.format(len(J[best_restart][: ]))
+            print 'Initial distortion : J = {:.2f}'.format(J[best_restart][0])
+            print 'Final distortion : J = {:.2f}'.format(J[best_restart][-1])
+            self.plot_distortion(J[best_restart])
+
+    def inference(self, X):
+        distances = np.sum(np.square(np.expand_dims(X, axis=1) - self.mu), axis=-1)
+        return np.argmin(distances, axis=-1)
+
+    def plot(self, X):
+        colors = ['blue', 'red', 'purple', 'darkred']
+        z = self.inference(X)
+
+        # Plot the decision boundary. For that, we will assign a color to each
+        # points of a meshgrid.
+        h = .02
+        x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+        y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+        xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+        Z = self.inference(np.c_[xx.ravel(), yy.ravel()])
+        Z = Z.reshape(xx.shape)
+        plt.figure(1)
+        plt.clf()
+        plt.imshow(Z, interpolation='nearest',
+                   extent=(xx.min(), xx.max(), yy.min(), yy.max()),
+                   cmap=plt.cm.Paired,
+                   aspect='auto', origin='lower')
+
+        # Plot the data points and centroids
+        for k in range(self.K):
+            plt.scatter(X[z == k,0], X[z == k,1], marker='^', c=colors[k])
+        plt.scatter(self.mu[:, 0], self.mu[:, 1], marker='x', c='white', s=400)
+        plt.show()
+        plt.close()
+
+    def plot_distortion(self, J):
+        plt.plot(J, linewidth=3)
+        plt.xlabel('Iterations')
+        plt.ylabel('Distortion')
+        plt.show()
+        plt.close()
+
+
+class GaussianMixture:
+    def __init__(self, K, covariance_hypothesis='spherical', iterations=50):
+        self.K = K
+        self.covariance_hypothesis = covariance_hypothesis
+        if self.covariance_hypothesis == 'spherical':
+            self.compute_tau = self._compute_tau_spherical
+            self.compute_sigma = self._compute_sigma_spherical
+        elif self.covariance_hypothesis == 'general':
+            self.compute_tau = self._compute_tau_general
+            self.compute_sigma = self._compute_sigma_general
+        self.pi = None
+        self.mu = None
+        self.sigma = None
+        self.iterations = iterations
+
+    def _compute_sigma_spherical(self, X, tau, mu):
+        return 0.5 * np.sum(tau * ((X-mu) * (X-mu)).sum(axis=1)) / np.sum(tau)
+
+    def _compute_tau_spherical(self, X, pi, mu, sigma):
+        return self._compute_tau_general(X, pi, mu, sigma * np.eye(2))
+
+    def _compute_sigma_general(self, X, tau, mu):
+        A = np.sqrt(np.expand_dims(tau, axis=-1)) * (X - mu)
+        return A.transpose().dot(A) / np.sum(tau)
+
+    def _compute_tau_general(self, X, pi, mu, sigma):
+        tau = np.zeros(X.shape[0])
+        A = np.linalg.inv(sigma)
+        for i in range(X.shape[0]):
+            tau[i] = (pi / np.sqrt(np.linalg.det(sigma))) * np.exp(-0.5 * (X[i, :] - mu).transpose().dot(A).dot(X[i, :] - mu))
+        return tau
+
+    def initialize_with_kmeans(self, X):
+        # Initialize mu with K-Means
+        kmeans = KMeans(self.K, initializer='random')
+        kmeans.train(X, render=False)
+        mu = kmeans.mu
+
+        # Initialize sigma and pi using cluster assignment of K-Means
+        z = kmeans.inference(X)
+        pi = np.bincount(z).astype(float) / z.size
+        if self.covariance_hypothesis == 'spherical':
+            sigma = np.zeros((self.K,))
+        elif self.covariance_hypothesis == 'general':
+            sigma = np.zeros((self.K, X.shape[1], X.shape[1]))
+
+        for k in range(self.K):
+            sigma[k] = self.compute_sigma(X, z==k, mu[k])
+
+        return pi, mu, sigma
+
+    def log_likelihood(self, X, pi, mu, sigma):
+        ll = np.zeros((X.shape[0], self.K))
+        for k in range(self.K):
+            ll[:, k] = self.compute_tau(X, pi[k], mu[k], sigma[k])
+        ll = np.sum(np.log(np.sum(ll, axis=1)))
+        return ll
+
+    def train(self, X, render=True):
+        ll = []
+        pi, mu, sigma = self.initialize_with_kmeans(X)
+        tau = np.zeros((X.shape[0], self.K))
+
+        for step in range(self.iterations):
+            # Compute log-likelihood
+            ll.append(self.log_likelihood(X, pi, mu, sigma))
+
+            # Compute soft assignment to cluster k, tau[i,k] (Expectation step)
+            for k in range(self.K):
+                tau[:,k] = self.compute_tau(X, pi[k], mu[k], sigma[k])
+            tau = tau / np.sum(tau, axis=-1, keepdims=True)
+
+            # Compute estimates for pi, mu, sigma based on the soft assignment (Maximization step)
+            pi = np.mean(tau, axis=0)
+            for k in range(self.K):
+                mu[k] = np.sum(np.expand_dims(tau[:,k], axis=-1) * X, axis=0) / np.sum(tau[:,k])
+                sigma[k] = self.compute_sigma(X, tau[:,k], mu[k])
+
+        self.pi = pi
+        self.mu = mu
+        self.sigma = sigma
+
+        if render:
+            print 'Initial likelihood : J = {:.2f}'.format(ll[0])
+            print 'Final likelihood : J = {:.2f}'.format(ll[-1])
+            self.plot_likelihood(ll)
+
+    def inference(self, X):
+        tau = np.zeros((X.shape[0], self.K))
+        for k in range(self.K):
+            tau[:, k] = self.compute_tau(X, self.pi[k], self.mu[k], self.sigma[k])
+        tau = tau / np.sum(tau, axis=-1, keepdims=True)
+        return tau
+
+    def plot(self, X):
+        colors = ['blue', 'red', 'purple', 'darkred']
+        z = np.argmax(self.inference(X), axis=-1)
+
+        # Compute the likelihood on the set
+        ll = self.log_likelihood(X, self.pi, self.mu, self.sigma)
+        print 'Likelihood : J = {:.2f}'.format(ll)
+
+        # Plot the decision boundary. For that, we will assign a color to each points of a meshgrid.
+        h = .1
+        x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+        y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+        xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+        Z = np.argmax(self.inference(np.c_[xx.ravel(), yy.ravel()]), axis=-1)
+        Z = Z.reshape(xx.shape)
+        plt.figure(1)
+        plt.clf()
+        plt.imshow(Z, interpolation='nearest',
+                   extent=(xx.min(), xx.max(), yy.min(), yy.max()),
+                   cmap=plt.cm.Paired,
+                   aspect='auto', origin='lower')
+
+        # Plot the data points, ellipses and centroids
+        splot = plt.subplot()
+        for k in range(self.K):
+            plt.scatter(X[z == k, 0], X[z == k, 1], marker='^', c=colors[k])
+            if self.covariance_hypothesis == 'spherical':
+                plot_ellipse(splot, self.mu[k], self.sigma[k] * np.eye(2), colors[k])
+            elif self.covariance_hypothesis == 'general':
+                plot_ellipse(splot, self.mu[k], self.sigma[k], colors[k])
+        plt.scatter(self.mu[:, 0], self.mu[:, 1], marker='x', c='white', s=400)
+        plt.axis('equal')
+        plt.show()
+        plt.close()
+
+    def plot_likelihood(self, ll):
+        plt.plot(ll, linewidth=3)
+        plt.title('Likelihood against the number of iterations')
+        plt.xlabel('Iterations')
+        plt.ylabel('Likelihood')
+        plt.show()
+        plt.close()
---
+++ assignment3/main.py
@@ -0,0 +1,248 @@
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from clustering import GaussianMixture, plot_ellipse
+from scipy.stats import multivariate_normal
+
+mpl.rcParams.update({'font.size': 30})
+
+
+def load_data(type='train'):
+    with open('data/EMGaussian.{}'.format(type)) as f:
+        x = []
+        for line in f.readlines():
+            line = line.strip('\n').split(' ')
+            x_i = [float(line[0]), float(line[1])]
+            x.append(x_i)
+    return np.asarray(x)
+
+
+def log_dot(a, log_p):
+    log_a = np.log(a)
+    max_log = np.amax(log_a + log_p)
+    return max_log + np.log(np.sum(np.exp(log_a + log_p - max_log)))
+
+
+def log_matrix_dot(A, log_p):
+    res = np.zeros((log_p.size,))
+    for k in range(res.size):
+        res[k] = log_dot(A[k, :], log_p)
+    return res
+
+
+class HMM:
+    def __init__(self, K, mu, sigma):
+        self.K = K
+        self.iterations = 5
+        # Markov chain parameters
+        self.pi = [0.25, 0.25, 0.25, 0.25]
+        self.A = 1. / 6. * np.ones((4, 4)) + 2. / 6. * np.eye(4)
+        # Gaussian parameters
+        self.mu = mu
+        self.sigma = sigma
+
+    def _alpha_recursion(self, X):
+        T = X.shape[0]
+        log_alphas = np.zeros((T, self.K))
+        log_multivariate_normal = lambda x: np.array(
+            [multivariate_normal.logpdf(x, self.mu[i], self.sigma[i]) for i in range(self.K)])
+
+        log_alphas[0, :] = np.log(self.pi) + log_multivariate_normal(X[0, :])
+
+        for t in range(1, T):
+            log_alphas[t, :] = log_matrix_dot(self.A, log_alphas[t - 1, :]) + log_multivariate_normal(X[t, :])
+
+        return log_alphas
+
+    def _beta_recursion(self, X):
+        T = X.shape[0]
+        log_betas = np.zeros((T, self.K))
+        log_multivariate_normal = lambda x: np.array(
+            [multivariate_normal.logpdf(x, self.mu[i], self.sigma[i]) for i in range(self.K)])
+
+        log_betas[-1, :] = 0.
+        for t in range(T - 2, -1, -1):
+            log_betas[t, :] = log_matrix_dot(self.A, log_betas[t + 1, :] + log_multivariate_normal(X[t + 1, :]))
+
+        return log_betas
+
+    def _decoding(self, X):
+        T = X.shape[0]
+
+        # Compute alpha and beta recursion
+        log_alphas = self._alpha_recursion(X)
+        log_betas = self._beta_recursion(X)
+
+        # Compute the log partition function
+        max_log = np.amax(log_alphas + log_betas, axis=1, keepdims=True)
+        log_part = max_log + np.log(np.sum(np.exp(log_alphas + log_betas - max_log), axis=1, keepdims=True))
+
+        # Compute the probability of q_t given the observed variables
+        log_gamma = log_alphas + log_betas - log_part
+
+        # Compute the probability of (q_t, q_t+1) given the observed variables
+        log_chsi = np.zeros((T - 1, self.K, self.K))
+        log_multivariate_normal = lambda x, k: multivariate_normal.logpdf(x, self.mu[k], self.sigma[k])
+        for t in range(T - 1):
+            for i in range(self.K):
+                for j in range(self.K):
+                    log_chsi[t, i, j] = log_alphas[t, i] + log_betas[t + 1, j] + np.log(
+                        self.A[i, j]) + log_multivariate_normal(X[t + 1, :], j) - log_part[t, 0]
+
+        return np.exp(log_gamma), np.exp(log_chsi)
+
+    def _compute_loglikelihood(self, X):
+        gamma, chsi = self._decoding(X)
+
+        log_multivariate_normal = lambda x: np.array(
+            [multivariate_normal.logpdf(x, self.mu[i], self.sigma[i]) for i in range(self.K)])
+        ll = np.sum(gamma * np.swapaxes(log_multivariate_normal(X), 0, 1)) + np.sum(
+            chsi * np.log(self.A[np.newaxis, :, :]))
+        return ll
+
+    def fit(self, X_train, X_test, render=True):
+        """
+        Run Expectation-Maximization algorithm to learn the parameters
+        """
+        T = X_train.shape[0]
+        ll = {'train': [], 'test': []}
+        for step in range(self.iterations):
+            # Compute log-likelihood
+            ll['train'].append(self._compute_loglikelihood(X_train))
+            ll['test'].append(self._compute_loglikelihood(X_test))
+
+            # E-step
+            gamma, chsi = self._decoding(X_train)
+
+            # M-step
+            self.pi = gamma[0, :]
+
+            for i in range(self.K):
+                # Update A
+                for j in range(self.K):
+                    self.A[i, j] = np.sum(chsi[:, i, j]) / np.sum(chsi[:, i, :])
+
+                # Update mu and sigma
+                self.mu[i] = np.sum(gamma[:, i, np.newaxis] * X_train, axis=0) / np.sum(gamma[:, i])
+
+                # Update sigma
+                self.sigma[i] = sum(
+                    gamma[t, i] * np.outer(X_train[t, :] - self.mu[i], X_train[t, :] - self.mu[i]) for t in
+                    range(T)) / np.sum(gamma[:, i])
+
+        if render:
+            self.plot_likelihood(ll)
+
+    def _viterbi_decoding(self, X):
+        T = X.shape[0]
+        log_multivariate_normal = lambda x: np.array(
+            [multivariate_normal.logpdf(x, self.mu[i], self.sigma[i]) for i in range(self.K)])
+
+        log_v = np.log(self.pi) + log_multivariate_normal(X[0, :])
+        paths = {k: {'log_v': log_v[k], 'states': [k]} for k in range(self.K)}
+
+        for t in range(1, T):
+            for k in range(self.K):
+                prev_state, prev_log_v = paths[k]['states'][-1], paths[k]['log_v']
+                log_v = log_multivariate_normal(X[t, :]) + np.log(self.A[prev_state, :]) + prev_log_v
+
+                paths[k]['states'].append(np.argmax(log_v))
+                paths[k]['log_v'] = np.amax(log_v)
+
+        best_path_key = np.argmax([paths[key]['log_v'] for key in paths])
+        return np.asarray(paths[best_path_key]['states'])
+
+    def plot(self, X):
+        colors = ['blue', 'red', 'purple', 'darkred']
+        gamma, chsi = self._decoding(X)
+        z = np.argmax(gamma, axis=-1)
+
+        # Plot the data points, ellipses and centroids
+        splot = plt.subplot()
+        for k in range(self.K):
+            plt.scatter(X[z == k, 0], X[z == k, 1], marker='^', c=colors[k])
+            plot_ellipse(splot, self.mu[k], self.sigma[k], colors[k])
+        plt.scatter(self.mu[:, 0], self.mu[:, 1], marker='x', c='white', s=400)
+        plt.axis('equal')
+        plt.show()
+        plt.close()
+
+    def plot_gamma(self, X, len=100, opt='proba'):
+        gamma, _ = self._decoding(X)
+
+        if opt == 'proba':
+            f, (ax1, ax2, ax3, ax4) = plt.subplots(4, sharex=True, sharey=True)
+            ax1.plot(gamma[:len, 0])
+            ax2.plot(gamma[:len, 1])
+            ax3.plot(gamma[:len, 2])
+            ax4.plot(gamma[:len, 3])
+        elif opt == 'states':
+            z = np.argmax(gamma, axis=-1)
+            plt.plot(z, 'ro')
+
+        plt.show()
+
+    def plot_viterbi(self, X):
+        colors = ['blue', 'red', 'purple', 'darkred']
+        z = self._viterbi_decoding(X)
+
+        # Plot the data points, ellipses and centroids
+        splot = plt.subplot()
+        for k in range(self.K):
+            plt.scatter(X[z == k, 0], X[z == k, 1], marker='^', c=colors[k])
+            plot_ellipse(splot, self.mu[k], self.sigma[k], colors[k])
+        plt.scatter(self.mu[:, 0], self.mu[:, 1], marker='x', c='white', s=400)
+        plt.axis('equal')
+        plt.show()
+        plt.close()
+
+    def plot_viterbi_states(self, X, len=100, opt='proba'):
+        states = self._viterbi_decoding(X)
+
+        if opt == 'proba':
+            f, (ax1, ax2, ax3, ax4) = plt.subplots(4, sharex=True, sharey=True)
+            ax1.plot(np.where(states[:len] == 0, 1, 0))
+            ax2.plot(np.where(states[:len] == 1, 1, 0))
+            ax3.plot(np.where(states[:len] == 2, 1, 0))
+            ax4.plot(np.where(states[:len] == 3, 1, 0))
+        elif opt == 'states':
+            plt.plot(states, 'ro')
+
+        plt.show()
+
+    def plot_likelihood(self, ll):
+        plt.plot(ll['test'], linewidth=3, label='Test')
+        plt.plot(ll['train'], linewidth=3, label='Train')
+        plt.title('Likelihood against the number of iterations')
+        plt.xlabel('Iterations')
+        plt.ylabel('Likelihood')
+        plt.legend()
+        plt.show()
+        plt.close()
+
+
+if __name__ == '__main__':
+    X_train = load_data()
+    X_test = load_data(type='test')
+
+    gm = GaussianMixture(4, 'general')
+    gm.train(X_train, render=False)
+
+    print "----- HMM -----"
+    hmm = HMM(4, gm.mu, gm.sigma)
+
+    # Plot gamma before training
+    hmm.plot_gamma(X_test, opt='proba')
+
+    # Learn the model
+    hmm.fit(X_train, X_test, render=True)
+
+    # Plot data with Viterbi decoding
+    hmm.plot_viterbi(X_test)
+
+    # Plot the marginal probabilities
+    hmm.plot_gamma(X_test, opt='proba')
+
+    # Plot the most likely states
+    hmm.plot_gamma(X_test, opt='states')
+    hmm.plot_viterbi_states(X_test, opt='states')
